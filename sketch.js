74df2ef781df2858d0e124fa7fc3ee9fc77142e1
HardhikErat <58391944+HardhikErat@users.noreply.github.com>
2020-04-08 22:00:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,5 @@
-// You could have multiple flags like: start, launch to indicate the state of the game.
-
 const { Engine, World, Bodies, Body, Mouse, MouseConstraint, Constraint, Composite, Detector } = Matter;
-// The above line creates different constant variables for Engine, World, Bodies etc.
 var engine,world;
-/*
-
-const {Engine} = Matter 
-is the same as 
-const Engine = Matter.Engine
-
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-*/
 var ground, ball;
 var bomb;
 var constraint;
@@ -45,10 +32,6 @@ function draw() {
     if(!(constraint.sling.bodyA)){
         ball.display();
     }
-    bomb.collide = b1;
-    bomb.collide = b2;
-    bomb.collide = b3;
-    bomb.collide = b4;
     shooter.display();
     noStroke();
     fill(255);
